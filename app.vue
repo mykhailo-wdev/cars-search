d243902934a2a7bdbbf4791a0c678f44c686ab38
Mike Mike <mike_insider@proton.me>
2024-09-23 12:15:26 +0300

    Created main layout

--- app.vue
+++ app.vue
@@ -5,5 +5,16 @@
       <a class="text-3xl font-mono">cartrader</a>
      </header>
     <!-- NAVBAR -->
+
+    <!-- HOME HERO -->
+     <div class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://newsroom.porsche.com/.imaging/mte/porsche-templating-theme/image_1290x726/dam/pnr/2024/Products/992-II/0840_nevada_coupe_u-crane_AKOS0607_edit_V03-sky.jpg/jcr:content/0840_nevada_coupe_u-crane_AKOS0607_edit_V03-sky.jpg')]">
+      <div class="z-10 absolute w-full h-full flex justify-center items-center">
+        <div class="text-center w-1/2">
+          <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
+        </div>
+      </div>
+      <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
+     </div>
+    <!-- HOME HERO -->
   </div>
 </template>
